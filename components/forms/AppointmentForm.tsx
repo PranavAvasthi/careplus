30ab63dd6967c501e0730c63b77e9fc954b87c07
PranavAvasthi <pranavavasthi44@gmail.com>
2024-09-23 15:00:28 +0530

    Admin Dashboard Completed

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -1,29 +1,41 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Dispatch, SetStateAction, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Form } from "@/components/ui/form";
+
+import { SelectItem } from "@/components/ui/select";
+import { Doctors } from "@/constants";
+import {
+  createAppointment,
+  updateAppointment,
+} from "@/lib/actions/appointment.actions";
+import { getAppointmentSchema } from "@/lib/validation";
+import { Appointment } from "@/types/appwrite.types";
+
+import "react-datepicker/dist/react-datepicker.css";
+
 import CustomFormField from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
-import { useState } from "react";
-import { getAppointmentSchema } from "@/lib/validation";
-import { useRouter } from "next/navigation";
+import { Form } from "../ui/form";
 import { FormFieldType } from "./PatientForm";
-import { Doctors } from "@/constants";
-import { SelectItem } from "../ui/select";
-import Image from "next/image";
 import { Status } from "@/types";
-import { createAppointment } from "@/lib/actions/appointment.actions";
 
 const AppointmentForm = ({
   userId,
   patientId,
-  type,
+  type = "create",
+  appointment,
+  setOpen,
 }: {
   userId: string;
   patientId: string;
-  type: "create" | "cancel" | "schedule";
+  type: "create" | "schedule" | "cancel";
+  appointment?: Appointment;
+  setOpen?: Dispatch<SetStateAction<boolean>>;
 }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
@@ -33,15 +45,19 @@ const AppointmentForm = ({
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-      primaryPhysician: "",
-      schedule: new Date(),
-      reason: "",
-      note: "",
-      cancellationReason: "",
+      primaryPhysician: appointment ? appointment?.primaryPhysician : "",
+      schedule: appointment
+        ? new Date(appointment?.schedule!)
+        : new Date(Date.now()),
+      reason: appointment ? appointment.reason : "",
+      note: appointment?.note || "",
+      cancellationReason: appointment?.cancellationReason || "",
     },
   });
 
-  async function onSubmit(values: z.infer<typeof AppointmentFormValidation>) {
+  const onSubmit = async (
+    values: z.infer<typeof AppointmentFormValidation>
+  ) => {
     setIsLoading(true);
 
     let status;
@@ -54,76 +70,96 @@ const AppointmentForm = ({
         break;
       default:
         status = "pending";
-        break;
     }
 
     try {
-        if(type === 'create' && patientId) {
-            const appointmentData = {
-                userId,
-                patient: patientId,
-                primaryPhysician: values.primaryPhysician,
-                schedule: new Date(values.schedule),
-                reason: values.reason!,
-                note: values.note,
-                status: status as Status,
-            }
+      if (type === "create" && patientId) {
+        const appointment = {
+          userId,
+          patient: patientId,
+          primaryPhysician: values.primaryPhysician,
+          schedule: new Date(values.schedule),
+          reason: values.reason!,
+          status: status as Status,
+          note: values.note,
+        };
 
-            const appointment = await createAppointment(appointmentData)
-            if(appointment) {
-                form.reset();
-                router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`);
-            }
+        const newAppointment = await createAppointment(appointment);
+
+        if (newAppointment) {
+          form.reset();
+          router.push(
+            `/patients/${userId}/new-appointment/success?appointmentId=${newAppointment.$id}`
+          );
         }
+      } else {
+        const appointmentToUpdate = {
+          userId,
+          appointmentId: appointment?.$id!,
+          appointment: {
+            primaryPhysician: values.primaryPhysician,
+            schedule: new Date(values.schedule),
+            status: status as Status,
+            cancellationReason: values.cancellationReason,
+          },
+          type,
+        };
 
+        // @ts-ignore
+        const updatedAppointment = await updateAppointment(appointmentToUpdate);
 
+        if (updatedAppointment) {
+          setOpen && setOpen(false);
+          form.reset();
+        }
+      }
     } catch (error) {
       console.log(error);
     }
-  }
+    setIsLoading(false);
+  };
 
   let buttonLabel;
   switch (type) {
     case "cancel":
-      buttonLabel = "Create Appointment";
-      break;
-    case "create":
-      buttonLabel = "Create Appointment";
+      buttonLabel = "Cancel Appointment";
       break;
     case "schedule":
-      buttonLabel = "Create Appointment";
+      buttonLabel = "Schedule Appointment";
       break;
     default:
-      break;
+      buttonLabel = "Submit Apppointment";
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
-          <h1 className="header">New Appointment</h1>
-          <p className="text-dark-700">
-            Request a new appointment in 10 seconds
-          </p>
-        </section>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
+        {type === "create" && (
+          <section className="mb-12 space-y-4">
+            <h1 className="header">New Appointment</h1>
+            <p className="text-dark-700">
+              Request a new appointment in 10 seconds.
+            </p>
+          </section>
+        )}
 
         {type !== "cancel" && (
           <>
             <CustomFormField
-              control={form.control}
               fieldType={FormFieldType.SELECT}
+              control={form.control}
               name="primaryPhysician"
               label="Doctor"
               placeholder="Select a doctor"
             >
-              {Doctors.map((doctor) => (
-                <SelectItem key={doctor.name} value={doctor.name}>
+              {Doctors.map((doctor, i) => (
+                <SelectItem key={doctor.name + i} value={doctor.name}>
                   <div className="flex cursor-pointer items-center gap-2">
                     <Image
                       src={doctor.image}
                       width={32}
                       height={32}
-                      alt={doctor.name}
+                      alt="doctor"
                       className="rounded-full border border-dark-500"
                     />
                     <p>{doctor.name}</p>
@@ -133,29 +169,35 @@ const AppointmentForm = ({
             </CustomFormField>
 
             <CustomFormField
-              control={form.control}
               fieldType={FormFieldType.DATE_PICKER}
+              control={form.control}
               name="schedule"
-              label="Expected Appointment Date"
+              label="Expected appointment date"
               showTimeSelect
-              dateFormat="MM/dd/yyyy -h:mm aa"
+              dateFormat="MM/dd/yyyy  -  h:mm aa"
             />
 
-            <div className="flex flex-col gap-6 xl:flex-row">
+            <div
+              className={`flex flex-col gap-6  ${
+                type === "create" && "xl:flex-row"
+              }`}
+            >
               <CustomFormField
-                control={form.control}
                 fieldType={FormFieldType.TEXTAREA}
+                control={form.control}
                 name="reason"
-                label="Reason For Appointment"
-                placeholder="Enter reason for appointment"
+                label="Appointment reason"
+                placeholder="Annual montly check-up"
+                disabled={type === "schedule"}
               />
 
               <CustomFormField
-                control={form.control}
                 fieldType={FormFieldType.TEXTAREA}
+                control={form.control}
                 name="note"
-                label="Additional Comments/Notes"
-                placeholder="Enter additional comments/notes"
+                label="Comments/notes"
+                placeholder="Prefer afternoon appointments, if possible"
+                disabled={type === "schedule"}
               />
             </div>
           </>
@@ -163,11 +205,11 @@ const AppointmentForm = ({
 
         {type === "cancel" && (
           <CustomFormField
-            control={form.control}
             fieldType={FormFieldType.TEXTAREA}
+            control={form.control}
             name="cancellationReason"
-            label="Reason For Cancellation"
-            placeholder="Enter reason for cancellation"
+            label="Reason for cancellation"
+            placeholder="Urgent meeting came up"
           />
         )}
 
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,13 +1,18 @@
 "use server";
 
-import { ID } from "node-appwrite";
+import { revalidatePath } from "next/cache";
+import { ID, Query } from "node-appwrite";
+
+import { Appointment } from "@/types/appwrite.types";
+
 import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
+  messaging,
 } from "../appwrite.config";
-import { parseStringify } from "../utils";
-import { CreateAppointmentParams } from "@/types";
+import { formatDateTime, parseStringify } from "../utils";
+import { CreateAppointmentParams, UpdateAppointmentParams } from "@/types";
 
 export const createAppointment = async (
   appointment: CreateAppointmentParams
@@ -20,22 +25,96 @@ export const createAppointment = async (
       appointment
     );
 
+    revalidatePath("/admin");
     return parseStringify(newAppointment);
   } catch (error) {
-    console.log(error);
+    console.error("An error occurred while creating a new appointment:", error);
   }
 };
 
-export const getAppointment = async(appointmentId: string) => {
-    try {
-        const appointment = await databases.getDocument(
-            DATABASE_ID!,
-            APPOINTMENT_COLLECTION_ID!,
-            appointmentId,
-        )
+export const getRecentAppointmentList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")]
+    );
 
-        return parseStringify(appointment);
-    } catch(error) {
-        console.log(error);
-    }
-}
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointment) => {
+        switch (appointment.status) {
+          case "scheduled":
+            acc.scheduledCount++;
+            break;
+          case "pending":
+            acc.pendingCount++;
+            break;
+          case "cancelled":
+            acc.cancelledCount++;
+            break;
+        }
+        return acc;
+      },
+      initialCounts
+    );
+
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    };
+
+    return parseStringify(data);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the recent appointments:",
+      error
+    );
+  }
+};
+
+export const updateAppointment = async ({
+  appointmentId,
+  userId,
+  appointment,
+  type,
+}: UpdateAppointmentParams) => {
+  try {
+    const updatedAppointment = await databases.updateDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId,
+      appointment
+    );
+
+    if (!updatedAppointment) throw Error;
+
+    revalidatePath("/admin");
+    return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.error("An error occurred while scheduling an appointment:", error);
+  }
+};
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+
+    return parseStringify(appointment);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the existing patient:",
+      error
+    );
+  }
+};
